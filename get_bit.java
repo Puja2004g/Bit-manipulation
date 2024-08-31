532953345c311574beb261755ca949084019829a
Puja2004g <pujaguchhait2004@gmail.com>
2024-09-01 01:12:21 +0530

    initial commit

---
+++ get_bit.java
@@ -0,0 +1,5 @@
+public class get_bit {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ left_and_right_shift.java
@@ -0,0 +1,14 @@
+public class left_and_right_shift {
+    public static void main(String[] args) {
+        int n= 10;
+
+        //left shift
+        int left = n>>1;
+
+        //right shift
+        int right = n<<1;
+
+        System.out.println("left = "+left);
+        System.out.println("right = "+right);
+    }
+}
